024a328e70218ade9178b2ddade70d7951c33229
Yusleidy Gamboa <129456753+Yusleidy20@users.noreply.github.com>
2024-08-19 21:09:46 -0500

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule], // Asegúrate de incluir FormsModule aquí
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular';
+  employeeArray: Employee[] = [
+    { id: 1, name: "Ryan", country: "USA" },
+    { id: 2, name: "Maria", country: "USA" },
+    { id: 3, name: "Santi", country: "USA" }
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    
+  if(this.selectedEmployee.id == 0){
+    this.selectedEmployee.id =  this.employeeArray.length + 1;
+    this.employeeArray.push(this.selectedEmployee);
+  }
+
+    
+    this.selectedEmployee = new Employee();
+  }
+
+  delete(){
+    this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+  }
+  selectEmployee(employee: Employee){
+    this.selectedEmployee = employee
+  }
 }
+ 
